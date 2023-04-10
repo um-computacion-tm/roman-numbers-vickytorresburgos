331fdfdea6d3b33dc0b9c547f0fab0c7f8e487c6
Victoria Torres <vickytorresburgos2@gmail.com>
2023-04-09 22:35:23 -0300

    Delete my_test.py

--- romanos/my_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from my_romanos import decimal2roman
-class MyTest(unittest.TestCase):
-    def test_I(self):
-        self.assertEqual(decimal2roman(1),"I")
-
-if __name__ == "__main__":
-    unittest.main()
